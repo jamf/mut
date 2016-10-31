b45983334359c0beea6b568521b2e78e7f5e0261
mike.levenick <levenimc@gmail.com>
2016-10-30 23:47:25 -0500

    Added AlamoFire

---
+++ Pods/Target Support Files/Pods-The MUT/Pods-The MUT-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_The_MUTVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_The_MUTVersionString[];
+
---
+++ Pods/Target Support Files/Pods-The MUTTests/Pods-The MUTTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_The_MUTTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_The_MUTTestsVersionString[];
+
