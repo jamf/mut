be3df57c9133fd03553f04ac48b6e517dd2d2cec
mike.levenick <levenimc@gmail.com>
2016-11-02 12:33:29 -0500

    handled early dismissals of help views

---
+++ The MUT/viewHelpCredentials.swift
@@ -0,0 +1,28 @@
+//
+//  viewHelpCredentials.swift
+//  The MUT
+//
+//  Created by Michael Levenick on 11/2/16.
+//  Copyright © 2016 Levenick Enterprises LLC. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class viewHelpCredentials: NSViewController {
+    
+    var disappeared: String!
+    
+    override func viewDidAppear() {
+        let mainQueue = DispatchQueue.main
+        let deadline = DispatchTime.now() + .seconds(7)
+        mainQueue.asyncAfter(deadline: deadline) {
+            if self.disappeared == nil {
+                self.dismissViewController(self)
+            }
+        }
+    }
+    override func viewWillDisappear() {
+        self.disappeared = "true"
+    }
+}
