a3cbb4091ea60b51c74eb68d9b7ea22418c03b4b
Michael Levenick <michael.levenick@gmail.com>
2019-05-24 12:57:18 -0500

    Cleaned up code in preparation for moving to v5

---
+++ The MUT/API.swift
@@ -0,0 +1,59 @@
+//
+//  API.swift
+//  The MUT v5
+//
+//  Created by Michael Levenick on 5/24/19.
+//  Copyright © 2019 Michael Levenick. All rights reserved.
+//
+
+import Foundation
+
+public class API {
+    
+    // This function can be used to generate a token. Pass in a URL and base64 encoded credentials.
+    // The credentials are inserted into the header.
+    public func generateToken(url: String, user: String, password: String) -> Data {
+        let dataMan = dataManipulation()
+        
+        let base64Credentials = dataMan.base64Credentials(user: user, password: password)
+        print("base64 creds: " + base64Credentials)
+        
+        // Declare a variable to be populated, and set up the HTTP Request with headers
+        var token = "nil".data(using: String.Encoding.utf8, allowLossyConversion: false)!
+        let encodedURL = NSURL(string: "\(url)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)! as URL
+        let semaphore = DispatchSemaphore(value: 0)
+        let request = NSMutableURLRequest(url: encodedURL)
+        request.httpMethod = "POST"
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Accept" : "application/json"]
+        let session = Foundation.URLSession(configuration: configuration)
+        
+        // Completion handler. This is what ensures that the response is good/bad
+        // and also what handles the semaphore
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) -> Void in
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode >= 199 && httpResponse.statusCode <= 299 {
+                    // Good response from API
+                    token = data!
+                    NSLog("[INFO  ]: Successful GET completed by The MUT.app")
+                    NSLog("[INFO  ]: " + response.debugDescription)
+                } else {
+                    // Bad Response from API
+                    token = data!
+                    NSLog("[ERROR ]: Failed GET completed by The MUT.app")
+                    NSLog("[ERROR ]: " + response.debugDescription)
+                }
+                semaphore.signal() // Signal completion to the semaphore
+            }
+            
+            if error != nil {
+                NSLog("[FATAL ]: " + error!.localizedDescription)
+            }
+        })
+        task.resume() // Kick off the actual GET here
+        semaphore.wait() // Wait for the semaphore before moving on to the return value
+        return token
+    }
+    
+}
