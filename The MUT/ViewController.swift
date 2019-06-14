7a9df195c041e403763d0bd51b9a6ef96c31f7ef
Ben Whitis <ben.whitis@jamf.com>
2019-06-14 14:49:25 -0500

    Implemented XML builder for userObject with CSV parsing

--- The MUT/ViewController.swift
+++ The MUT/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import CSV
+import Foundation
 
 class ViewController: NSViewController, URLSessionDelegate, DataSentDelegate {
     
@@ -104,6 +106,39 @@ class ViewController: NSViewController, URLSessionDelegate, DataSentDelegate {
 
     }
     
+    
+    func readCSV(pathToCSV: String) -> [[String]]{
+        
+        print("begin readCSV...")
+        let stream = InputStream(fileAtPath: pathToCSV)!
+        var csvArray = [[String]]()
+        let csv = try! CSVReader(stream: stream)
+        while let row = csv.next() {
+            print("\(row)")
+            csvArray = (csvArray + [row])
+        }
+        
+        return csvArray
+    }
+    
+    @IBAction func btnPreFlightAction(_ sender: Any) {
+        print("Begin specificLine...")
+        let csvArray = readCSV(pathToCSV: self.globalPathToCSV.path!)
+        print("Number of rows including header: \(csvArray.count)")
+        print("Array of Arrays containing each row: \(csvArray)")
+        print("")
+        print("Printing Array 0 of the csvArray: \(csvArray[0])")
+        print("")
+        print("Printing Array 1 of the csvArray: \(csvArray[1])")
+        for row in 1...(csvArray.count - 1) {
+            let currentRow = csvArray[row]
+            print("csvArray: \(csvArray[row])")
+            print("Username is: \(currentRow[0])")
+            print("...")
+            xmlMan.userObject(username: currentRow[0], full_name: currentRow[1], email_address: currentRow[2], phone_number: currentRow[3], position: currentRow[4], ldap_server: currentRow[5])
+        }
+        
+    }
     
     @IBAction func btnExportCSV(_ sender: Any) {
         print("exporting CSV")
