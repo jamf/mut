fc823b5a96fa2adc6ba66160d257a8e10619ba58
Michael Levenick <michael.levenick@gmail.com>
2019-06-15 00:11:05 -0500

    Moved the submit updates into its own function and fixed various bugs

--- The MUT/ViewController.swift
+++ The MUT/ViewController.swift
@@ -45,17 +45,10 @@ class ViewController: NSViewController, URLSessionDelegate, DataSentDelegate {
     var globalBase64: String!
     
     func userDidAuthenticate(base64Credentials: String, url: String, token: String, expiry: Int) {
-        //code
-        print("It ran on main view")
-        print(base64Credentials)
-        print(url)
-        print(token)
-        print(expiry)
         globalExpiry = expiry
         globalToken = token
         globalURL = url
         globalBase64 = base64Credentials
-        
     }
     
     let dataMan = dataManipulation()
@@ -103,54 +96,28 @@ class ViewController: NSViewController, URLSessionDelegate, DataSentDelegate {
                 self.txtCSV.stringValue = self.globalPathToCSV.path!
             }
         }
+    }
 
-    }
-    
-    
-    func readCSV(pathToCSV: String) -> [[String]]{
-        
-        print("begin readCSV...")
-        let stream = InputStream(fileAtPath: pathToCSV)!
-        var csvArray = [[String]]()
-        let csv = try! CSVReader(stream: stream)
-        while let row = csv.next() {
-            print("\(row)")
-            csvArray = (csvArray + [row])
-        }
-        
-        return csvArray
-    }
     
     @IBAction func btnPreFlightAction(_ sender: Any) {
-        print("Begin specificLine...")
-        let csvArray = readCSV(pathToCSV: self.globalPathToCSV.path!)
-        /*print("Number of rows including header: \(csvArray.count)")
-        print("Array of Arrays containing each row: \(csvArray)")
-        print("")
-        print("Printing Array 0 of the csvArray: \(csvArray[0])")
-        print("")
-        print("Printing Array 1 of the csvArray: \(csvArray[1])")
-         */
-        for row in 1...(csvArray.count - 1) {
-            let currentRow = csvArray[row]
-            //print("csvArray: \(csvArray[row])")
-            print("Username is: \(currentRow[0])")
-            print("...")
-            let xmlToPut = xmlMan.userObject(username: currentRow[0], full_name: currentRow[1], email_address: currentRow[2], phone_number: currentRow[3], position: currentRow[4], ldap_server: currentRow[5])
-            var encodedURL = NSURL(string: "https://bwhitis.jamfcloud.com/JSSResource/users/name/\(currentRow[0])")! as URL
-            let response = APIFunc.putData(passedurl: encodedURL, credentials: globalBase64, endpoint: "", allowUntrusted: true, xmlToPut: xmlToPut)
-            print("Attempted to PUT \(response)")
-        }
+        submitUpdates()
         
     }
     
     @IBAction func btnExportCSV(_ sender: Any) {
-        print("exporting CSV")
         NSLog("[INFO  : Saving CSV Templates to User's Download's Directory")
         CSVMan.ExportCSV()
-        
-        print("export of CSV done")
     }
-    
-    
+
+    func submitUpdates() {
+        NSLog("[INFO  : Beginning parsing the CSV file into the array stream.")
+        let csvArray = CSVMan.readCSV(pathToCSV: self.globalPathToCSV.path ?? "/dev/null")
+        let headerRow = csvArray[0]
+        let numberOfColumns = headerRow.count
+        for row in 1...(csvArray.count - 1) {
+            let currentRow = csvArray[row]
+            let xmlToPut = xmlMan.userObject(username: currentRow[0], full_name: currentRow[1], email_address: currentRow[2], phone_number: currentRow[3], position: currentRow[4], ldap_server: currentRow[5])
+            let response = APIFunc.putData(passedUrl: globalURL, credentials: globalBase64, endpoint: "users", identifierType: "name", identifier: currentRow[0], allowUntrusted: false, xmlToPut: xmlToPut)
+        }
+    }
 }
