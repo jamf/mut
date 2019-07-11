9448b19145a2ade845a7b0e6ecec7c2fa3f1f56e
Michael Levenick <michael.levenick@gmail.com>
2019-07-10 23:16:29 -0500

    New class to assist with building JSON files for submission via the API.

---
+++ The MUT/jsonBuilder.swift
@@ -0,0 +1,26 @@
+//
+//  jsonBuilder.swift
+//  MUT
+//
+//  Created by Michael Levenick on 7/9/19.
+//  Copyright © 2019 Levenick Enterprises LLC. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+public class jsonManager {
+    public func buildJson(versionLock: Int, serialNumbers: [String]) -> Data{
+        // Array & Dictionary
+        var jsonToReturn: Data? = "".data(using: .utf8)
+        let json: JSON =  ["serialNumbers": serialNumbers, "versionLock": versionLock]
+        print(json.description)
+        do {
+            // Parse the JSON to return token and Expiry
+            jsonToReturn = try json.rawData()
+        } catch let error as NSError {
+            NSLog("[ERROR ]: Failed to get data from jsonToReturn" + error.debugDescription)
+        }
+        return jsonToReturn ?? "".data(using: .utf8)!
+    }
+}
