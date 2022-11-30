2d4e3518c4299d2bf6c29a4ddd572dbf56f5bcca
mikelevenick <mike.levenick@jamf.com>
2022-11-30 15:22:25 -0600

    Updates to keychain helper

--- The MUT/KeychainHelper.swift
+++ The MUT/KeychainHelper.swift
@@ -10,33 +10,42 @@ import Foundation
 
 class KeyChainHelper {
 
-    class func save(key: String, data: Data) -> OSStatus {
-        let query = [
-            kSecClass as String       : kSecClassGenericPassword as String,
-            kSecAttrAccount as String : key,
-            kSecValueData as String   : data ] as [String : Any]
-
+    class func save(username: String, password: String, server: String) throws {
+        let passData = password.data(using: String.Encoding.utf8)!
+        var query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrAccount as String: username,
+                                    kSecAttrServer as String: server,
+                                    kSecAttrLabel as String: KeyVars.key, // This is the key we will to find it later
+                                    kSecValueData as String: passData]
         SecItemDelete(query as CFDictionary)
-
-        return SecItemAdd(query as CFDictionary, nil)
+        let status  = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status)}
     }
-
-    class func load(key: String) -> Data? {
-        let query = [
-            kSecClass as String       : kSecClassGenericPassword,
-            kSecAttrAccount as String : key,
-            kSecReturnData as String  : kCFBooleanTrue!,
-            kSecMatchLimit as String  : kSecMatchLimitOne ] as [String : Any]
-
-        var dataTypeRef: AnyObject? = nil
-
-        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
-
-        if status == noErr {
-            return dataTypeRef as! Data?
-        } else {
-            return nil
+    
+    class func load() throws {
+        // Build the query for what to find
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrLabel as String: KeyVars.key, // Looking to match this key
+                                    kSecMatchLimit as String: kSecMatchLimitOne, // Limiting to one result
+                                    kSecReturnAttributes as String: true,
+                                    kSecReturnData as String: true]
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status != errSecItemNotFound else { throw KeychainError.noPassword }
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+        
+        guard let existingItem = item as? [String : Any],
+            let passwordData = existingItem[kSecValueData as String] as? Data,
+            let password = String(data: passwordData, encoding: String.Encoding.utf8),
+            let username = existingItem[kSecAttrAccount as String] as? String,
+            let server = existingItem[kSecAttrServer as String] as? String
+        else {
+            throw KeychainError.unexpectedPasswordData
         }
+        Credentials.server = server
+        Credentials.password = password
+        Credentials.username = username
     }
     
     class func delete(key: String) -> OSStatus {
@@ -65,12 +74,19 @@ public struct Credentials {
     static var username:  String?
     static var password: String?
     static var server: String?
+    static var base64Encoded: String?
 }
 
 public struct Token {
     static var value: String?
     static var expiration: Int?
     static var data: Data?
+}
+
+enum KeychainError: Error {
+    case noPassword
+    case unexpectedPasswordData
+    case unhandledError(status: OSStatus)
 }
 
 extension Data {
@@ -84,4 +100,3 @@ extension Data {
         return self.withUnsafeBytes { $0.load(as: T.self) }
     }
 }
-
