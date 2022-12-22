63aa32e8cea64f53bb763e1b7f9c196ca390cd4c
mikelevenick <mike.levenick@jamf.com>
2022-12-21 22:58:56 -0600

    Updated menucontroller

--- The MUT/Settings Menu/MenuHelper.swift
+++ The MUT/Settings Menu/MenuHelper.swift
@@ -10,13 +10,45 @@ import Cocoa
 import Foundation
 
 public class Setting {
-    let name: String! = nil
-    let value: Any? = nil
-    let storage: NSButton! = nil
     
-    init(name: String!, value: Any?, storage: NSButton!){
-        self.name = name
+    let settingDefaults = UserDefaults.standard
+    
+    var key: String! = nil
+    var value: Any? = nil
+    
+    init(key: String!, value: Any?){
+        self.key = key
         self.value = value
-        self.storage = storage
+    }
+    
+    // These singleton values should only be updated on application load and when menu options are clicked
+    
+    // Standard Defaults Storage
+    static var username = Setting(key: "UserName", value: nil)
+    static var instanceURL = Setting(key: "InstanceURL", value: nil)
+    
+    // General Settings
+    static var logLevel = Setting(key: "LogLevel", value: nil)
+    static var delimiter = Setting(key: "Delimiter", value: nil)
+    static var usersInts = Setting(key: "UserInts", value: nil)
+    
+    // Security Settings
+    static var allowUntrustedSSL = Setting(key: "AllowUntrusted", value: nil)
+    static var rememberInstance = Setting(key: "RememberInstance", value: nil)
+    static var rememberUser = Setting(key: "RememberUser", value: nil)
+    
+    static let allDefaults = [username,
+                              instanceURL,
+                              logLevel,
+                              delimiter,
+                              usersInts,
+                              allowUntrustedSSL,
+                              rememberInstance,
+                              rememberUser]
+    
+    public func restoreDefaults(){
+        for setting in Setting.allDefaults{
+            setting.value = settingDefaults.value(forKey: setting.key)
+        }
     }
 }
